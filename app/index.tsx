dddf5eca3ca643edc1b4ad79c34784d9f8a7e1eb
Ewan <ewan.hamon@gmail.com>
2024-08-12 16:19:10 +0200

    feat: add some fonts

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import * as Font from 'expo-font';
+import Home from "./screens/home";
+import * as SplashScreen from 'expo-splash-screen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+const getFonts = () => Font.loadAsync({
+  'outfit-regular': require('../assets/fonts/Outfit-Regular.ttf'),
+  'outfit-bold': require('../assets/fonts/Outfit-Bold.ttf'),
+});
+
+export default function Index() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadResources = async () => {
+      await getFonts();
+      setFontsLoaded(true);
+      await SplashScreen.hideAsync();
+    };
+
+    loadResources();
+  }, []);
+
+  return <Home />;
+}
