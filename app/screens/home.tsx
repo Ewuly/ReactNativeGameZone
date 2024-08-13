977d5d7db68885db5bc815b1f9db9e06e0cd3a31
Ewan <ewan.hamon@gmail.com>
2024-08-13 16:32:24 +0200

    feat: create modal

--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, Modal } from "react-native";
 import { globalStyles } from "../styles/global";
 import Card from "../shared/card";
+import { MaterialIcons } from '@expo/vector-icons';
+import { StyleSheet } from "react-native";
 
 export default function Home({ navigation }: { navigation: any }) {
+
+
+    const [modalOpen, setModalOpen] = useState(false);
+
     const [reviews, setReviews] = useState([
         { title: 'Pizza', rating: 5, body: 'lorem ipsum', key: '1' },
         { title: 'Burger', rating: 4, body: 'lorem ipsum', key: '2' },
@@ -14,12 +20,32 @@ export default function Home({ navigation }: { navigation: any }) {
 
     return (
         <View style={globalStyles.container}>
+
+            <Modal visible={modalOpen} animationType="slide">
+                <View style={styles.modalContent}>
+                    <MaterialIcons
+                        name='close'
+                        size={24}
+                        style={{ ...styles.modalToggle, ...styles.modalClose }}
+                        onPress={() => setModalOpen(false)}
+                    />
+                    <Text>Hello</Text>
+                </View>
+            </Modal>
+
+            <MaterialIcons
+                name='add'
+                size={24}
+                style={styles.modalToggle}
+                onPress={() => setModalOpen(true)}
+            />
+
             <FlatList
                 data={reviews}
                 renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item )}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
                         <Card>
-                            <Text style={globalStyles.titleText}>{ item.title }</Text>
+                            <Text style={globalStyles.titleText}>{item.title}</Text>
                         </Card>
                     </TouchableOpacity>
 
@@ -28,3 +54,21 @@ export default function Home({ navigation }: { navigation: any }) {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    modalContent: {
+        flex: 1,
+    },
+    modalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center',
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0,
+    }
+})
